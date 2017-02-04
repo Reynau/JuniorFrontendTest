044efa279eba3e61d9eca7915625908db1d0ae3d
Reynau <xavi.lopez.reynau@gmail.com>
2017-02-04 14:29:01 +0100

    Add full functionality

--- js/client.js
+++ js/client.js
@@ -1,5 +1,18 @@
 var resultElem = document.getElementById("result");
 var errorElem = document.getElementById("error");
+var usernameElem = document.getElementById("username");
+var fullnameElem = document.getElementById("fullname");
+var bioElem = document.getElementById("bio");
+var listElem = document.getElementById("repo-list");
+var imgElem = document.getElementById("image");
+
+document.getElementById('searchInput')
+    .addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode == 13) {
+        document.getElementById('searchBtn').click();
+      }
+    });
 
 function search () {
   var search_value = document.getElementById("searchInput").value;
@@ -31,15 +44,67 @@ function search () {
 }
 
 function processData (user_json, repos_json) {
+  // Clean all data
+  cleanChilds();
+
+  // Show result div
   resultElem.style.display = 'block';
   errorElem.style.display = 'none';
 
-  console.log(user_json);
-  console.log(repos_json);
+  // Add profile img
+  var img = new Image();
+  img.src = user_json.avatar_url;
+  img.setAttribute("align", "left");
+  imgElem.appendChild(img);
+
+  // Add profile data
+  var username = document.createTextNode('@' + user_json.login);
+  var fullname = document.createTextNode(user_json.name);
+  var bio = document.createTextNode(user_json.bio);
+  usernameElem.appendChild(username);
+  fullnameElem.appendChild(fullname);
+  bioElem.appendChild(bio);
+
+  for (var i = 0; i < repos_json.length; ++i) {
+    var entry = document.createElement('li');
+    var left = document.createElement('div');
+    var right = document.createElement('div');
+
+    entry.className = 'wrapper';
+    left.className = 'left';
+    right.className = 'right';
+
+    left.appendChild(document.createTextNode(repos_json[i].name));
+    right.innerHTML = '<i class="fa fa-star" aria-hidden="true"></i>&nbsp;' + repos_json[i].stargazers_count +
+        '&nbsp;<i class="fa fa-code-fork" aria-hidden="true"></i>&nbsp;' + repos_json[i].forks_count + '&nbsp;';
+
+    entry.appendChild(left);
+    entry.appendChild(right);
+
+    listElem.appendChild(entry);
+  }
 }
 
+function cleanChilds () {
+  // Clean imgs
+  while (imgElem.firstChild)
+    imgElem.removeChild(imgElem.firstChild);
+
+  while (usernameElem.firstChild)
+    usernameElem.removeChild(usernameElem.firstChild);
+  while (fullnameElem.firstChild)
+    fullnameElem.removeChild(fullnameElem.firstChild);
+  while (bioElem.firstChild)
+    bioElem.removeChild(bioElem.firstChild);
+
+  while (listElem.firstChild)
+    listElem.removeChild(listElem.firstChild)
+}
 
 function searchNotFound () {
+  // Clean all data
+  cleanChilds();
+
   resultElem.style.display = 'none';
   errorElem.style.display = 'block';
 }
