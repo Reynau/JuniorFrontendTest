ead69ed3854128bcb9e075806167e43d31a635ec
Reynau <xavi.lopez.reynau@gmail.com>
2017-02-05 13:47:23 +0100

    Refactorize code

--- js/client.js
+++ js/client.js
@@ -17,29 +17,37 @@ document.getElementById('searchInput')
 function search () {
   var search_value = document.getElementById("searchInput").value;
   if (!search_value) return;
-  var url = "https://api.github.com/users/" + search_value;
+  searchProfile(search_value);
+}
 
+function searchProfile (user) {
+  var url = "https://api.github.com/users/" + user;
+  var ret = getProfile(url);
+}
+
+function getProfile (url) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
     if (xmlHttp.readyState == 4) {
-      if (xmlHttp.status == 200) {
-        var user_json = JSON.parse(xmlHttp.responseText);
-        xmlHttp.onreadystatechange = function() {
-          if (xmlHttp.readyState == 4) {
-            if (xmlHttp.status == 200) {
-              var repos_json = JSON.parse(xmlHttp.responseText);
-              processData(user_json, repos_json);
-            }
-            else searchNotFound();
-          }
-        };
-        xmlHttp.open("GET", url + "/repos", true); // true for asynchronous
-        xmlHttp.send(null);
-      }
+      if (xmlHttp.status == 200)
+        xmlRepos = getRepos(JSON.parse(xmlHttp.responseText));
       else searchNotFound();
     }
   };
-  xmlHttp.open("GET", url, true); // true for asynchronous
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send(null);
+}
+
+function getRepos (profile_json) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4) {
+      if (xmlHttp.status == 200)
+        processData(profile_json, JSON.parse(xmlHttp.responseText));
+      else searchNotFound();
+    }
+  };
+  xmlHttp.open("GET", profile_json.repos_url, true);
   xmlHttp.send(null);
 }
 
